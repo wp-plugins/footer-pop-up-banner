4d42df52ffb615ad9de66b162470ac09cc45c7ed
ninjapress <ninjapress@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-31 06:50:52 +0000

    tagging version 1.1
    git-svn-id: https://plugins.svn.wordpress.org/footer-pop-up-banner/tags/1.1@976005 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ templates/banner.php
@@ -0,0 +1,17 @@
+
+
+<script language="javascript">
+
+   var fpub_delay = <?= get_option('fpub_delay'); ?>;
+   var fpub_persist = <?= get_option('fpub_persist'); ?>;
+
+</script>
+
+<div id="fpub-popup" class="trg-overlay hide small" style="background-color: <?= hex2rgba(get_option('fpub_background_colr'), get_option('fpub_background_fade') / 100); ?>; border-top: 1px solid <?= get_option('fpub_line_color'); ?>">
+   <a href="<?= get_option('fpub_link'); ?>">
+      <img src="<?= get_option('fpub_image'); ?>" height="<?= get_option('fpub_height'); ?>" width="<?= get_option('fpub_width'); ?>" />
+   </a>
+   <?php if (get_option('fpub_btn_close') == 'on') : ?>
+      <a class="close-overlay" >&#215; </a>
+   <?php endif; ?>
+</div>
