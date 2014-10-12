1d0478889f102e97bf5704ff0a6706e0723b6d7e
ninjapress <ninjapress@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-12 07:23:48 +0000

    Upgrade Version 1.4
    git-svn-id: https://plugins.svn.wordpress.org/footer-pop-up-banner/tags/1.4@1005929 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/banner.js
@@ -0,0 +1,23 @@
+jQuery(document).ready(function() {
+    jQuery('#fpub-popup').hide();
+    
+    setTimeout('open_banner()', fpub_delay * 1000);
+
+    jQuery('.close-overlay').click(function() {
+        jQuery('#fpub-popup').hide('slow');
+    });
+}); 
+
+function open_banner() {
+    jQuery('#fpub-popup').show('slow');
+
+    if (fpub_persist !== null && fpub_persist !== undefined) {
+        if (fpub_persist > 0) {
+            setTimeout('close_banner()', fpub_persist * 1000)
+        }
+    }
+}
+
+function close_banner() {
+    jQuery('#fpub-popup').hide('slow');
+}
---
+++ templates/banner.php
@@ -0,0 +1,16 @@
+<script language="javascript">
+
+   var fpub_delay = <?= is_numeric(get_option('fpub_delay')) ? get_option('fpub_delay') : 0; ?>;
+   var fpub_persist = <?= is_numeric(get_option('fpub_persist')) ? get_option('fpub_persist') : 0; ?>;
+
+</script>
+
+<div id="fpub-popup" class="trg-overlay hide small" style="background-color: <?= hex2rgba(get_option('fpub_background_colr'), get_option('fpub_background_fade') / 100); ?>; border-top: 1px solid <?= get_option('fpub_line_color'); ?>">
+   <?php if (get_option('fpub_btn_close') == 'on') : ?>
+      <a class="close-overlay" >&#215; </a>
+   <?php endif; ?>
+   
+   <a href="<?= get_option('fpub_link'); ?>">
+      <img src="<?= get_option('fpub_image'); ?>" height="<?= get_option('fpub_height') > 0 ? get_option('fpub_height') . 'px' : ''; ?>" width="<?= get_option('fpub_width') > 0 ? get_option('fpub_width') . 'px' : ''; ?>" />
+   </a>
+</div>
